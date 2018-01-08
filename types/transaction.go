a6d97f17973b31a1ce7663ec4ebef07a2b852f95
decanus <dean.eigenmann@icloud.com>
2018-01-08 19:31:11 +0100

    added bson

--- types/transaction.go
+++ types/transaction.go
@@ -3,12 +3,12 @@ package types
 import "github.com/ethereum/go-ethereum/common"
 
 type Transaction struct {
-	TransactionID string         `json:"tx"`
-	OrderHash     string         `json:"hash"`
-	BlockNumber   int            `json:"block"`
-	Timestamp     string         `json:"timestamp"`
-	Taker         common.Address `json:"taker"`
-	Maker         common.Address `json:"maker"`
-	Give          Trade          `json:"give"`
-	Get           Trade          `json:"get"`
+	TransactionID string         `json:"tx" bson:"tx"`
+	OrderHash     string         `json:"hash" bson:"hash"`
+	BlockNumber   int            `json:"block" bson:"block"`
+	Timestamp     string         `json:"timestamp" bson:"timestamp"`
+	Taker         common.Address `json:"taker" bson:"taker"`
+	Maker         common.Address `json:"maker" bson:"maker"`
+	Give          Trade          `json:"give" bson:"give"`
+	Get           Trade          `json:"get" bson:"get"`
 }
