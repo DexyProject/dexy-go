433563812b856e4fa0c7c4d68902d4837f9adfc0
Niranjan Ravichandra <niranjan96dxb@gmail.com>
2018-01-11 13:56:13 -0500

    spaces

--- validators/balancevalidator.go
+++ validators/balancevalidator.go
@@ -17,10 +17,10 @@ type BalanceValidatorSession struct {
 
 }
 func (balanceSession *BalanceValidatorSession) CheckBalance(tokenAddr, userAddr common.Address) (*big.Int, error) {
-	token, err:= exchange.NewExchangeInterface(tokenAddr, balanceSession.conn)//Can pass nil for CallOpts and TransactOpts
+	exchange, err := exchange.NewExchangeInterface(tokenAddr, balanceSession.conn)//Can pass nil for CallOpts and TransactOpts
 	if err != nil {
 		return nil, fmt.Errorf("could not connect to contract session")
 	}
 
-	return token.BalanceOf(nil,tokenAddr,userAddr)
+	return exchange.BalanceOf(nil, tokenAddr, userAddr)
 }
