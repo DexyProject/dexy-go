371799ca698076234dc836569cc7639179703caa
Niranjan Ravichandra <niranjan96dxb@gmail.com>
2018-01-19 21:23:09 +0400

    cleanup of names

--- cmd/orderbook/main.go
+++ cmd/orderbook/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/DexyProject/dexy-go/handlers"
 	"github.com/DexyProject/dexy-go/orderbook"
 	"github.com/gorilla/mux"
+	"github.com/DexyProject/dexy-go/db"
 )
 
 func main() {
@@ -14,11 +15,12 @@ func main() {
 	r := mux.NewRouter()
 
 	ob := &orderbook.MemoryOrderBook{}
+	h := &db.MongoTickQueries{}
 
 	getorders := handlers.GetOrdersHandler{OrderBook: ob}
 	getorder := handlers.GetOrderHandler{OrderBook: ob}
 	createorder := handlers.CreateOrderHandler{OrderBook: ob}
-	getticks := handlers.CreateHistoryHandler
+	getticks := handlers.GetTicksHandler{TickQuery: h}
 
 	r.HandleFunc("/orders", getorders.Handle).Methods("GET").Queries("token", "")
 	r.HandleFunc("/orders", createorder.Handle).Methods("POST")
