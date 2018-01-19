5a59c4839de49b856614923c2737b6b47caa468a
decanus <dean.eigenmann@icloud.com>
2018-01-19 20:34:23 +0100

    minor changes

--- cmd/orderbook/main.go
+++ cmd/orderbook/main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/DexyProject/dexy-go/handlers"
 	"github.com/DexyProject/dexy-go/orderbook"
 	"github.com/gorilla/mux"
+	"fmt"
 )
 
 func main() {
 
+	defer deferOnPanic()
 
-	ob, err := orderbook.NewMongoOrderBook("localhost") // @todo
+	ob, err := orderbook.NewMongoOrderBook(os.Args[1]) // @todo
 	if err != nil {
 		log.Fatal("Mongo error", err)
 	}
@@ -25,11 +28,16 @@ func main() {
 	r.HandleFunc("/orders", getorders.Handle).Methods("GET").Queries("token", "")
 	r.HandleFunc("/orders", createorder.Handle).Methods("POST")
 	r.HandleFunc("/orders/{order}", getorder.Handle).Methods("GET")
+	http.Handle("/", r)
 
-	//http.Handle("/", r)
-
-	err := http.ListenAndServe(":12312", r)
+	err = http.ListenAndServe(":12312", r)
 	if err != nil {
 		log.Fatal("Listen:", err)
+	}
+}
+
+func deferOnPanic() {
+	if err := recover(); err != nil {
+		fmt.Println(err)
 	}
 }
