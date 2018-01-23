6673107884d4e9d70003b1c85bc6e203409e6f34
Niranjan Ravichandra <niranjan96dxb@gmail.com>
2018-01-23 14:20:42 +0400

    added endpoint for tick queries

--- endpoints/ticks.go
+++ endpoints/ticks.go
@@ -10,13 +10,13 @@ type Ticks struct {
 	TickQuery db.TickQuery
 }
 
-func (ticks *TickQuery) GetTicks(rw http.ResponseWriter, r *http.Request) {
+func (ticks *Ticks) GetTicks(rw http.ResponseWriter, r *http.Request) {
 	rw.Header().Set("Content-Type", "application/json")
 
 	query := r.URL.Query()
 	block := query.Get("block")
 
-	h, err := ticks.TickQuery.AggregateTick(block)
+	h, err := ticks.TickQuery.AggregateTick(block) //Output of AggregateTick is bson.M[]
 	if err != nil {
 		// @todo error handling
 		rw.WriteHeader(http.StatusBadRequest)
