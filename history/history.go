ae4a3d21fb721cdb6410ecb6bf6a79dab4bc9e4f
decanus <dean.eigenmann@icloud.com>
2018-01-24 14:06:09 +0100

    Added some skeleton stuff + todos

--- history/history.go
+++ history/history.go
@@ -6,4 +6,5 @@ import (
 
 type History interface {
 	GetHistory(token types.Address, user *types.Address, limit int) []types.Transaction
+	InsertTransaction(transaction types.Transaction) error
 }
--- watchers/tradewatcher.go
+++ watchers/tradewatcher.go
@@ -1,10 +1,28 @@
 package watchers
 
-type TradeWatcher struct {
+import (
+	"github.com/DexyProject/dexy-go/history"
+	"github.com/DexyProject/dexy-go/types"
+)
 
+type TradeWatcher struct {
+	history history.History
 }
 
 func (tf *TradeWatcher) Watch() error {
 	// @todo read for trade events,
 	return nil
+}
+
+func (tf *TradeWatcher) HandleTransaction(transaction types.Transaction) {
+
+	err := tf.history.InsertTransaction(transaction)
+	if err != nil {
+		// @todo handle
+		return
+	}
+
+	// @todo updated filled values
+	// @todo delete if amount == filled
+
 }
