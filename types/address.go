fa9445059b83857f4a9209083b8ebd094575c639
decanus <dean.eigenmann@icloud.com>
2018-01-24 13:00:22 +0100

    Fixed marshalling

--- types/address.go
+++ types/address.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"encoding/json"
 	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -8,11 +9,12 @@ import (
 )
 
 type Address struct {
-	common.Address
+	*common.Address
 }
 
 func HexToAddress(hex string) Address {
-	return Address{Address: common.HexToAddress(hex)}
+	addr := common.HexToAddress(hex)
+	return Address{Address: &addr}
 }
 
 func (a Address) GetBSON() (interface{}, error) {
@@ -20,16 +22,22 @@ func (a Address) GetBSON() (interface{}, error) {
 }
 
 func (a *Address) SetBSON(raw bson.Raw) error {
-
-	var addr string
-	err := raw.Unmarshal(addr)
+	var s string
+	err := raw.Unmarshal(&s)
 	if err != nil {
 		return err
 	}
 
-	return a.UnmarshalJSON([]byte(addr))
+	addr := common.HexToAddress(s)
+	a.Address = &addr
+
+	return nil
 }
 
 func (a Address) IsZero() bool {
 	return a.String() == "0x0000000000000000000000000000000000000000"
+}
+
+func (a *Address) MarshalJSON() ([]byte, error) {
+	return json.Marshal(a.Address.String())
 }
