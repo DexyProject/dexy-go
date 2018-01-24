5fd1ed0ddcacc43bdc4e3c6b5bb8e73e3feb76c7
decanus <dean.eigenmann@icloud.com>
2018-01-24 13:08:00 +0100

    updated

--- types/int.go
+++ types/int.go
@@ -4,6 +4,7 @@ import (
 	"gopkg.in/mgo.v2/bson"
 	"math/big"
 	"github.com/ethereum/go-ethereum/accounts/abi"
+	"fmt"
 )
 
 type Int struct {
@@ -19,12 +20,18 @@ func (x Int) GetBSON() (interface{}, error) {
 }
 
 func (x *Int) SetBSON(raw bson.Raw) error {
+	var val string
+	err := raw.Unmarshal(&val)
+	if err != nil {
+		return err
+	}
 
-	//var addr string
-	//err := raw.Unmarshal(addr)
-	//if err != nil {
-	//	return err
-	//}
+	num, ok := new(big.Int).SetString(val, 10)
+	if !ok {
+		return fmt.Errorf("could not set string")
+	}
+
+	x.Int = *num
 
 	return nil
 }
