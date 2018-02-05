c17bd55b84f29411bb1df04cc52b6e3ab12fd188
decanus <dean.eigenmann@icloud.com>
2018-02-05 10:38:09 +0100

    fixed

--- types/tick.go
+++ types/tick.go
@@ -1,14 +1,12 @@
 package types
 
-import "github.com/DexyProject/dexy-go/types"
-
 type Tick struct {
-	Block     int64     `json:"block" bson:"block"`
-	OpenTime  int64     `json:"opentime" bson:"opentime"`
-	CloseTime int64     `json:"closetime" bson:"closetime"`
-	Volume    int64     `json:"volume" bson:"volume"`
-	Open      types.Int `json:"open" bson:"open"`
-	Close     types.Int `json:"close" bson:"close"`
-	High      types.Int `json:"high" bson:"high"`
-	Low       types.Int `json:"low" bson:"low"`
+	Block     int64 `json:"block" bson:"block"`
+	OpenTime  int64 `json:"opentime" bson:"opentime"`
+	CloseTime int64 `json:"closetime" bson:"closetime"`
+	Volume    int64 `json:"volume" bson:"volume"`
+	Open      Int   `json:"open" bson:"open"`
+	Close     Int   `json:"close" bson:"close"`
+	High      Int   `json:"high" bson:"high"`
+	Low       Int   `json:"low" bson:"low"`
 }
