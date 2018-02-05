b4c12bb23f36819819a2d11c3fc373b0054e1068
decanus <dean.eigenmann@icloud.com>
2018-02-05 11:55:55 +0100

    Added new data type

--- types/ec.go
+++ types/ec.go
@@ -8,27 +8,17 @@ import (
 )
 
 type EC struct {
-	V        int    `json:"v" bson:"v"`
-	R        string `json:"r" bson:"r"`
-	S        string `json:"s" bson:"s"`
-	Prefixed bool   `json:"prefixed" bson:"prefixed"`
+	V        int   `json:"v" bson:"v"`
+	R        Bytes `json:"r" bson:"r"`
+	S        Bytes `json:"s" bson:"s"`
+	Prefixed bool  `json:"prefixed" bson:"prefixed"`
 }
 
 func (ec *EC) Verify(address Address, hash Hash) bool {
 
-	r, err := StringToBytes(ec.R)
-	if err != nil {
-		return false
-	}
-
-	s, err := StringToBytes(ec.S)
-	if err != nil {
-		return false
-	}
-
 	sigBytes := make([]byte, 65)
-	copy(sigBytes[32-len(r):32], r[:])
-	copy(sigBytes[64-len(s):64], s[:])
+	copy(sigBytes[32-len(ec.R):32], ec.R[:])
+	copy(sigBytes[64-len(ec.S):64], ec.S[:])
 	sigBytes[64] = byte(ec.V - 27)
 
 	var hashedBytes = hash[:]
