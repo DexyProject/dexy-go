55fecaeb0f7ae544f8d8a78d7354a94f94bd4000
decanus <dean.eigenmann@icloud.com>
2018-02-14 22:04:54 +0100

    added some stuff

--- types/transaction.go
+++ types/transaction.go
@@ -1,12 +1,13 @@
 package types
 
 type Transaction struct {
-	TransactionID Bytes   `json:"tx" bson:"tx"`
-	OrderHash     Hash    `json:"hash" bson:"hash"`
-	BlockNumber   uint64   `json:"block" bson:"block"`
-	Timestamp     int64   `json:"timestamp" bson:"timestamp"`
-	Taker         Address `json:"taker" bson:"taker"`
-	Maker         Address `json:"maker" bson:"maker"`
-	Give          Trade   `json:"give" bson:"give"`
-	Get           Trade   `json:"get" bson:"get"`
+	TransactionID    Bytes   `json:"tx" bson:"tx"`
+	TransactionIndex uint    `json:"index" bson:"index"`
+	OrderHash        Hash    `json:"hash" bson:"hash"`
+	BlockNumber      uint64  `json:"block" bson:"block"`
+	Timestamp        Int     `json:"timestamp" bson:"timestamp"`
+	Taker            Address `json:"taker" bson:"taker"`
+	Maker            Address `json:"maker" bson:"maker"`
+	Give             Trade   `json:"give" bson:"give"`
+	Get              Trade   `json:"get" bson:"get"`
 }
--- watchers/tradewatcher.go
+++ watchers/tradewatcher.go
@@ -5,12 +5,15 @@ import (
 	"github.com/DexyProject/dexy-go/history"
 	"github.com/DexyProject/dexy-go/orderbook"
 	"github.com/DexyProject/dexy-go/types"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 type TradeWatcher struct {
 	history   history.History
 	exchange  exchange.ExchangeInterface
 	orderbook orderbook.OrderBook
+	ethereum  ethclient.Client
 }
 
 func (tf *TradeWatcher) Watch() error {
@@ -23,26 +26,44 @@ func (tf *TradeWatcher) Watch() error {
 		return err // @todo better
 	}
 
+	var timestamp types.Int
+	var block common.Hash
+
 	for {
 
+		// @todo make async see to handle sub errors from first return val
 		trade := <-sink
 
+		// @todo cleanup
+		if block != trade.Raw.BlockHash {
+			b, err := tf.ethereum.HeaderByHash(nil, block)
+			if err != nil {
+				// @todo
+			}
+
+			block = trade.Raw.BlockHash
+			timestamp = types.Int{Int: *b.Time}
+		}
+
 		tx := types.Transaction{
-			TransactionID: types.Bytes{Bytes: trade.Raw.TxHash.Bytes()},
-			OrderHash: trade.Hash,
-			BlockNumber: trade.Raw.BlockNumber,
-			Timestamp: 0,
-			Taker: types.Address{Address: trade.Taker},
-			Maker: types.Address{Address: trade.Maker},
+			TransactionID:    types.Bytes{Bytes: trade.Raw.TxHash.Bytes()},
+			TransactionIndex: trade.Raw.Index,
+			OrderHash:        trade.Hash,
+			BlockNumber:      trade.Raw.BlockNumber,
+			Timestamp:        timestamp,
+			Taker:            types.Address{Address: trade.Taker},
+			Maker:            types.Address{Address: trade.Maker},
 			Give: types.Trade{
-				Token: types.Address{Address: trade.TokenGive},
-				Amount: trade.AmountGive,
+				Token:  types.Address{Address: trade.TokenGive},
+				Amount: types.Int{Int: *trade.AmountGive},
 			},
 			Get: types.Trade{
-				Token: types.Address{Address: trade.TokenGet},
-				Amount: trade.AmountGet,
+				Token:  types.Address{Address: trade.TokenGet},
+				Amount: types.Int{Int: *trade.AmountGet},
 			},
 		}
+
+		go tf.handleTransaction(tx)
 
 	}
 
@@ -72,7 +93,7 @@ func (tf *TradeWatcher) handleTransaction(transaction types.Transaction) {
 	tf.orderbook.UpdateOrderFilledAmount(transaction.OrderHash, filled)
 }
 
-func (tf *TradeWatcher) isOrderFilled(order types.Hash, amount types.Int) (bool) {
+func (tf *TradeWatcher) isOrderFilled(order types.Hash, amount types.Int) bool {
 	o := tf.orderbook.GetOrderByHash(order)
 	return o.Get.Amount.Cmp(&amount.Int) == 0
 }
