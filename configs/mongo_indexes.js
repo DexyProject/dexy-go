3bf27229c260c66cf39ef236fab287b153828ff2
decanus <dean.eigenmann@icloud.com>
2018-02-21 12:16:36 +0100

    added

---
+++ configs/mongo_indexes.js
@@ -0,0 +1,7 @@
+use OrderBook;
+
+db.Orders.createIndex({ hash: 1 }, {unique: true});
+db.Orders.createIndex({ expires: 1 }, {expireAfterSeconds: 0});
+db.Orders.createIndex({ price: 1 });
+db.Orders.createIndex({ get: {token: 1}});
+db.Orders.createIndex({ give: {token: 1} });
