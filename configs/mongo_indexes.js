142b52a5b98c5064a5eb409289abb104758957f5
decanus <dean.eigenmann@icloud.com>
2018-02-21 12:20:48 +0100

    updated

--- configs/mongo_indexes.js
+++ configs/mongo_indexes.js
@@ -1,6 +1,5 @@
 use OrderBook;
 
-db.Orders.createIndex({ hash: 1 }, {unique: true});
 db.Orders.createIndex({ expires: 1 }, {expireAfterSeconds: 0});
 db.Orders.createIndex({ price: 1 });
 db.Orders.createIndex({ "get.token": 1 });
