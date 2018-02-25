0bd0314d0784a7529652dd277174bb1bdf6ae798
Dean Eigenmann <dean@tokenate.io>
2018-02-25 23:11:21 +0100

    enhancement/remove-price (#56)

--- types/order.go
+++ types/order.go
@@ -24,7 +24,7 @@ type Orders struct {
 
 type Order struct {
 	Hash      Hash      `json:"hash,omitempty" bson:"_id"`
-	Price     string    `json:"price,omitempty" bson:"price"`
+	Price     string    `json:"-" bson:"price"`
 	Give      Trade     `json:"give" bson:"give"`
 	Get       Trade     `json:"get" bson:"get"`
 	Expires   Timestamp `json:"expires" bson:"expires"`
