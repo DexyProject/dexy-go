bd1b6f68b28a8ad928f2145d0c79329d5381f843
decanus <dean.eigenmann@icloud.com>
2018-03-08 11:32:18 +0100

    added todos

---
+++ watchers/fundswatcher.go
@@ -0,0 +1,33 @@
+package watchers
+
+import "github.com/DexyProject/dexy-go/contracts"
+
+type FundsWatcher struct {
+	vault contracts.Vault
+}
+
+func (fw *FundsWatcher) Watch() {
+}
+
+func (fw *FundsWatcher) handleWithdraw() {
+
+	withdrawn := 0
+
+
+	// @todo call on orders
+
+	onOrders := 0
+	balance := 0
+
+	if onOrders < balance {
+		// @todo updated db
+	}
+}
+
+func (fw *FundsWatcher) handleDeposit() {
+
+	// @todo calculate the amount which is underfunded
+
+	// @todo update mongo daatabase
+
+}
