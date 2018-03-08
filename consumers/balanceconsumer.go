0f4a3478ad6facc84b38587e294c6f9b50aa281c
decanus <dean.eigenmann@icloud.com>
2018-03-08 06:12:09 +0100

    actual channel implementations

--- consumers/balanceconsumer.go
+++ consumers/balanceconsumer.go
@@ -1,32 +1,47 @@
 package consumers
 
 import (
-	"github.com/DexyProject/dexy-go/contracts"
-	"log"
 	"fmt"
+	"log"
+
+	"github.com/DexyProject/dexy-go/contracts"
+	"github.com/ethereum/go-ethereum/event"
 )
 
 type BalanceConsumer struct {
 	vault *contracts.Vault
+
+	stop chan struct{}
+
+	withdraw, deposit event.Subscription
 }
 
 func (bc *BalanceConsumer) StartConsuming() error {
 
-	deposit, err := bc.vault.WatchDeposited(nil, nil, nil) // @todo
+	deposited := make(chan *contracts.VaultDeposited)
+	deposit, err := bc.vault.WatchDeposited(nil, deposited, nil)
 	if err != nil {
 		return fmt.Errorf("failed to watch deposited: %s", err.Error())
 	}
 
-	withdraw, err := bc.vault.WatchWithdrawn(nil, nil, nil) // @todo
+	withdrawn := make(chan *contracts.VaultWithdrawn)
+	withdraw, err := bc.vault.WatchWithdrawn(nil, withdrawn, nil)
 	if err != nil {
 		return fmt.Errorf("failed to watch withdrawn: %s", err.Error())
 	}
+
+	bc.withdraw = withdraw
+	bc.deposit = deposit
+
+	go bc.consume(deposited, withdrawn)
 
 	return nil
 }
 
 func (bc *BalanceConsumer) StopConsuming() {
-	panic("implement me")
+	bc.withdraw.Unsubscribe()
+	bc.deposit.Unsubscribe()
+	close(bc.stop)
 }
 
 func (bc *BalanceConsumer) consume(deposited chan *contracts.VaultDeposited, withdrawn chan *contracts.VaultWithdrawn) {
@@ -36,6 +51,8 @@ func (bc *BalanceConsumer) consume(deposited chan *contracts.VaultDeposited, wit
 			log.Print(deposit)
 		case withdraw := <-withdrawn:
 			log.Print(withdraw)
+		case <-bc.stop:
+			return
 		}
 	}
 }
