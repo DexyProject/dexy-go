582fa7a36191a69b430fdc11b9a39a579183bb8d
decanus <dean.eigenmann@icloud.com>
2018-03-09 12:21:39 +0100

    Updated index

--- configs/mongo_indexes.js
+++ configs/mongo_indexes.js
@@ -4,9 +4,14 @@ db.orders.createIndex({ expires: 1 }, {expireAfterSeconds: 0});
 db.orders.createIndex({ price: 1 });
 db.orders.createIndex({ "get.token": 1 });
 db.orders.createIndex({ "give.token": 1 });
+db.orders.createIndex({ user: 1});
 
 db.history.createIndex({ timestamp: 1 });
 db.history.createIndex({ block: 1 });
+db.history.createIndex({ maker: 1 });
+db.history.createIndex({ taker: 1 });
+db.history.createIndex({ "get.token": 1 });
+db.history.createIndex({ "give.token": 1 });
 
 db.ticks.createIndex({ "pair.quote": 1 });
 db.ticks.createIndex({ timestamp: 1 });
