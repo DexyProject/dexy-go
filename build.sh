20862a60766eaf0ace714a091aee0dfeafda9af6
decanus <dean.eigenmann@icloud.com>
2018-04-04 14:05:11 +0200

    fix

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ go build -o out/api cmd/rest/main.go
 go build -o out/cancelledwatcher cmd/cancelledwatcher/main.go
 go build -o out/tradewatcher cmd/tradewatcher/main.go
 go build -o out/tickworker cmd/tickworker/main.go
+go build -o out/marketsworker cmd/marketsworker/main.go
