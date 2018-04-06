3c1d87cf5a045ef5d38847eaf84671fe9c722506
Ivo Georgiev <ivo@linvo.org>
2018-04-06 18:18:41 +0300

    Revert "build.sh: always compile for linux, static binaries"
    This reverts commit 5c3a9ccb74205fd7dc330644a72bd595a412abc6.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-CGO_ENABLED=0 GOOS=linux go build -o out/api cmd/rest/main.go 
-CGO_ENABLED=0 GOOS=linux go build -o out/cancelledwatcher cmd/cancelledwatcher/main.go
-CGO_ENABLED=0 GOOS=linux go build -o out/tradewatcher cmd/tradewatcher/main.go
-CGO_ENABLED=0 GOOS=linux go build -o out/tickworker cmd/tickworker/main.go
-CGO_ENABLED=0 GOOS=linux go build -o out/marketsworker cmd/marketsworker/main.go
+go build -o out/api cmd/rest/main.go 
+go build -o out/cancelledwatcher cmd/cancelledwatcher/main.go
+go build -o out/tradewatcher cmd/tradewatcher/main.go
+go build -o out/tickworker cmd/tickworker/main.go
+go build -o out/marketsworker cmd/marketsworker/main.go
