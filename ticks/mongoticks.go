e087f1abb5d35230b0d8eb3a1712a70f0858cc83
decanus <dean.eigenmann@icloud.com>
2018-04-07 12:28:33 +0200

    2 new functions wip

--- ticks/mongoticks.go
+++ ticks/mongoticks.go
@@ -26,8 +26,8 @@ func NewMongoTicks(connection string) (*MongoTicks, error) {
 	return &MongoTicks{session: session}, nil
 }
 
-func (tq *MongoTicks) InsertTicks(ticks []types.Tick) error {
-	session := tq.session.Clone()
+func (t *MongoTicks) InsertTicks(ticks []types.Tick) error {
+	session := t.session.Clone()
 	defer session.Close()
 
 	c := session.DB(DBName).C(FileName)
@@ -43,8 +43,8 @@ func (tq *MongoTicks) InsertTicks(ticks []types.Tick) error {
 	return nil
 }
 
-func (tq *MongoTicks) FetchTicks(token types.Address) ([]types.Tick, error) {
-	session := tq.session.Clone()
+func (t *MongoTicks) FetchTicks(token types.Address) ([]types.Tick, error) {
+	session := t.session.Clone()
 	defer session.Close()
 
 	c := session.DB(DBName).C(FileName)
@@ -58,29 +58,38 @@ func (tq *MongoTicks) FetchTicks(token types.Address) ([]types.Tick, error) {
 	return results, nil
 }
 
-func (tq *MongoTicks) FetchLatestTickForTokens(tokens []types.Address) (map[types.Address]types.Tick, error) {
-	session := tq.session.Clone()
-	defer session.Close()
+//func (t *MongoTicks) FetchLatestTickForTokens(tokens []types.Address) (map[types.Address]types.Tick, error) {
+//	session := t.session.Clone()
+//	defer session.Close()
+//
+//	c := session.DB(DBName).C(FileName)
+//	results := make(map[types.Address]types.Tick, 0)
+//
+//	pipe := []bson.M{
+//		{"$match": bson.M{"pair.quote": bson.M{"$in": tokens}}},
+//		{"$sort": bson.M{"timestamp": -1}},
+//		{"$group": bson.M{"_id": "$pair.quote", "ticks": bson.M{"$push": "$$ROOT"}}},
+//		{"$replaceRoot": bson.M{"newRoot": bson.M{"$arrayElemAt": []interface{}{"$ticks", 0}}}},
+//	}
+//
+//	ticks := make([]types.Tick, 0)
+//	err := c.Pipe(pipe).All(&ticks)
+//	if err != nil {
+//		return results, err
+//	}
+//
+//	for _, tick := range ticks {
+//		results[tick.Pair.Quote] = tick
+//	}
+//
+//	return results, nil
+//}
 
-	c := session.DB(DBName).C(FileName)
-	results := make(map[types.Address]types.Tick, 0)
+func (t *MongoTicks) FetchAggregateVolumeForTokens(tokens []types.Address) (map[types.Address]types.Int, error) {
+	// @todo
+}
 
-	pipe := []bson.M{
-		{"$match": bson.M{"pair.quote": bson.M{"$in": tokens}}},
-		{"$sort": bson.M{"timestamp": -1}},
-		{"$group": bson.M{"_id": "$pair.quote", "ticks": bson.M{"$push": "$$ROOT"}}},
-		{"$replaceRoot": bson.M{"newRoot": bson.M{"$arrayElemAt": []interface{}{"$ticks", 0}}}},
-	}
 
-	ticks := make([]types.Tick, 0)
-	err := c.Pipe(pipe).All(&ticks)
-	if err != nil {
-		return results, err
-	}
-
-	for _, tick := range ticks {
-		results[tick.Pair.Quote] = tick
-	}
-
-	return results, nil
+func (t *MongoTicks) FetchLatestCloseForTokens(tokens []types.Address) (map[types.Address]types.Int, error) {
+	// @todo
 }
