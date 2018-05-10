e2a8958a43d62f10298e9a803ca47aaf4f0bf473
decanus <dean.eigenmann@icloud.com>
2018-05-10 09:56:31 +0200

    added func, not sure if needed

--- math/math.go
+++ math/math.go
@@ -8,6 +8,10 @@ import (
 )
 
 func ToUnitAmount(amount types.Int, decimals uint8) float64 {
-	a, _ := new(big.Float).SetInt(&amount.Int).Float64()
+	return ToUnitAmountForFloat(*new(big.Float).SetInt(&amount.Int), decimals)
+}
+
+func ToUnitAmountForFloat(amount big.Float, decimals uint8) float64 {
+	a, _ := amount.Float64()
 	return a / math.Pow(10.0, float64(decimals))
 }
