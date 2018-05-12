740d6d18999063c88a20514b29cc1d00e571a7f9
decanus <dean.eigenmann@icloud.com>
2018-05-12 10:45:29 +0200

    added tests

--- types/int_test.go
+++ types/int_test.go
@@ -3,20 +3,34 @@ package types
 import (
 	"testing"
 	"strconv"
+	"fmt"
 )
 
 func TestInt_UnmarshalJSON(t *testing.T) {
+	val := 10
+	i := new(Int)
 
+	bytes := []byte(fmt.Sprintf("\"%d\"", val))
+
+	i.UnmarshalJSON(bytes)
+
+	if i.String() != strconv.Itoa(val) {
+		t.Errorf("%s does not equal expected %d", i.String(), val)
+	}
+}
+
+func TestInt_MarshalJSON(t *testing.T) {
 	val := int64(10)
 	i := NewInt(val)
 
-	j, _ := i.MarshalJSON()
+	str, err := i.MarshalJSON()
+	if err != nil {
+		t.Error(err)
+	}
 
-	ni := new(Int)
+	expected := fmt.Sprintf("\"%d\"", val)
 
-	ni.UnmarshalJSON(j)
-
-	if ni.String() != strconv.Itoa(int(val)) {
-		t.Errorf("%s does not equal expected %d", ni.String(), val)
+	if string(str) != expected {
+		t.Errorf("%s does not equal to expected %s", string(str), expected)
 	}
 }
